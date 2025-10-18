269ddcf66d8f21e998ec187288a9294666f98a9f
rinshadkl <riinshad18@gmail.com>
2025-10-18 06:56:49 +0530

    Add custom 404 error page component

--- src/Pages/Posters.jsx
+++ src/Pages/Posters.jsx
@@ -1,8 +1,7 @@
-import React, { useMemo } from 'react'
+import React from 'react'
 import ImageCard from '../components/custom/imagecard/ImageCard'
-import { Badge } from '../components/ui/badge'
 import { posters } from '@/lib/posters'
-import { Image, Layers } from 'lucide-react'
+import { motion } from "motion/react"
 
 const Posters = () => {
 
@@ -10,8 +9,14 @@ const Posters = () => {
     <div className="py-10 px-4 md:px-8 lg:px-16 min-h-screen">
       {/* Posters Grid */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 max-w-7xl mx-auto">
-        {posters.slice().reverse().map(poster => (
-          <ImageCard key={poster.id} image={poster.image} title={poster.title} className={"shadow-2xl"} />
+        {posters.slice().reverse().map((poster, index) => (
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+          >
+            <ImageCard image={poster.image} title={poster.title} />
+          </motion.div>
         ))}
       </div>
     </div>
